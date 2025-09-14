5fb509c35f826625734e39a16dd510e036ce1c39
gudipatidheeraj <gudipatidheeraj@gmail.com>
2025-09-14 11:45:54 +0530

    to test webhooks in jenkins

---
+++ summm.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class summm {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 59;
+		int sum = 0;
+		sum += a + b;
+		System.out.println("Value of a is", +a);
+		System.out.println("Value of b is", +b);
+		System.out.println("Value of adding both a and b sum is:" +sum);
+	}
+}
